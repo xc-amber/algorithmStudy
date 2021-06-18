67372d6e808501fd881eddef9c776feaa56436a6
xc <690336375@qq.com>
2021-06-19 00:56:36 +0800

    Leetcode No.189题解和代码

---
+++ array/Solution189.java
@@ -0,0 +1,44 @@
+/*
+* 1.暴力法：从1到k次，每次先用tmp变量保存nums[length - 1]的值，然后从数组右边界开始遍历，使得右边的元素=左边的元素；
+* 最后使得nums[0] = temp就完成了交换
+* 时间复杂度分析：需要循环k次，每次循环需要遍历数组所有元素，所以时间复杂度O(n) = 数组元素个数n*k；
+* 空间复杂度分析：每次循环需要一个tmp变量，O(n) = n;
+* 该方法运行超时；
+* 代码如下：
+*     public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        for (int i = 1; i <= k; i++) {
+            int temp = nums[len - 1];
+            for (int j = len - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = temp;
+        }
+    }
+
+* 方法2：
+* 新建一个数组int[] numsNew长度和原数组相等；
+* 要右转k步，所以从原数组的nums[length - k]开始遍历到nums[length - 1],依次赋值给numsNew的前k个元素
+* 再从原数组的nums[0]遍历到nums[length - k - 1];依次赋值给numsNew[k]d到numsNew[length - 1];
+* 该方法遇到测试用例k大于nums的长度时，就会有数组越界的问题；k大于length时，相当于已经旋转超过一轮了
+* 由超过一轮这个想法发现如果k == length的话，相当于没有翻转，直接返回就行
+* 所以如果k> length的话，只要令k = k % length再处理就行
+* 时间复杂度分析:遍历一次数组长度，O(n) = 数组长度n；
+* 空间复杂度分析：新建了一个数组，O(n) = 1；
+* */
+public class Solution189 {
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        int[] numsNew = new int[len];
+        int time = k;
+        if(k > len){
+            time = k % len;
+        }
+        if(time == 0){
+            return;
+        }
+        if (time >= 0) System.arraycopy(nums, len - time, numsNew, 0, time);
+        if (len - time >= 0) System.arraycopy(nums, 0, numsNew, time, len - time);
+        System.arraycopy(numsNew, 0, nums, 0, len);
+    }
+}
