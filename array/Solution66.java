c74fdb0ca7ae5c89830bb1a3b525fb0dd0b0be1f
xc <690336375@qq.com>
2021-06-20 17:21:39 +0800

    Leetcode No.66题解和代码

--- array/Solution66.java
+++ array/Solution66.java
@@ -13,30 +13,42 @@ Leetcode No.66
 * 当原数组的最后一个元素为9的时候
 * 例如原数组为{1,2,9}，最后一个元素变为0，且前一个元素需要+1，即为{1,3,0}
 * 但如果原数组的第二个元素也为9的时候依然要加1进1；
-* 所以从最后一个元素下标len - 1开始起比较这个元素是否为9，如果不为9的话就直接加1break循环；
-* 如果为9的话，前一位元素就递归比较，(len - 1)--，(len - 1) < 0 时跳出循环；(len - 1)-- == len--;
-* 以上比较结束后，判断修改后原数组的起始元素是否为0，如果为0的话，就是原数组每个元素都为0；所以新建一个原数组长度+1的数组，第一个元素为1，后面每个元素为0
-* 如果起始元素不为0，直接返回修改后的原数组
+* 所以从最后一个元素下标len - 1开始起比较这个元素是否为9，如果不为9的话就直接加1,然后return修改后的数组结束；
+* 如果为9的话，再进行前一位元素的比较，(len - 1)--，(len - 1) < 0 时跳出循环；(len - 1)-- == len--;
+* 以上比较结束后还没return的话，就是原数组每个元素都为9；所以新建一个原数组长度+1的数组，第一个元素为1，后面每个元素为0
+*代码如下：
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        while (len - 1 >= 0){
+            if(digits[len - 1] != 9){
+                digits[len - 1] ++;
+                return digits;
+            }else {
+                digits[len - 1] = 0;
+                len--;
+            }
+        }
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            return result;
+    }
+*
+
 * */
 public class Solution66 {
     public int[] plusOne(int[] digits) {
         int len = digits.length;
         while (len - 1 >= 0){
             if(digits[len - 1] != 9){
-                digits[len - 1] += 1;
-                break;
+                digits[len - 1] ++;
+                return digits;
             }else {
                 digits[len - 1] = 0;
                 len--;
             }
         }
-        if (digits[0] == 0) {
             int[] result = new int[digits.length + 1];
             result[0] = 1;
-            System.arraycopy(result, 1, digits, 0, digits.length);
             return result;
-        }else{
-            return digits;
-        }
     }
 }
