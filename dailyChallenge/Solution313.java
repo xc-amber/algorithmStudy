568c671569a79f66f5cec4599bd37059b66976cd
xc <690336375@qq.com>
2021-08-09 23:07:47 +0800

    LeetCode No.313题解和代码

---
+++ dailyChallenge/Solution313.java
@@ -0,0 +1,30 @@
+import java.util.*;
+/*
+* 方法1： 动态规划
+* 详细题解参考官解
+* */
+
+public class Solution313 {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        int m = primes.length;
+        int[] pointers = new int[m];
+        Arrays.fill(pointers, 1);
+        for (int i = 2; i <= n; i++) {
+            int[] nums = new int[m];
+            int minNum = Integer.MAX_VALUE;
+            for (int j = 0; j < m; j++) {
+                nums[j] = dp[pointers[j]] * primes[j];
+                minNum = Math.min(minNum, nums[j]);
+            }
+            dp[i] = minNum;
+            for (int j = 0; j < m; j++) {
+                if (minNum == nums[j]) {
+                    pointers[j]++;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
